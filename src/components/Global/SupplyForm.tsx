9569e906d78160d68af2914210eff9f6afde9493
s1avo <s.nadolny@hotmail.com>
2023-02-23 19:50:17 +0100

    Change the quantity form handling from useForm to formik.

--- src/components/Global/SupplyForm.tsx
+++ src/components/Global/SupplyForm.tsx
@@ -1,48 +1,19 @@
-import React, { useEffect, useState } from "react";
-import { useAppDispatch, useAppSelector } from "../../app/redux-hooks";
+import React from "react";
+import { useAppSelector } from "../../app/redux-hooks";
 import * as Yup from "yup";
-import { useForm } from "react-hook-form";
-import { yupResolver } from "@hookform/resolvers/yup";
+
 import {
   Box,
   Button,
-  Divider,
-  FormControl,
-  InputLabel,
   MenuItem,
   Modal,
-  Select,
-  SelectChangeEvent,
+  Paper,
   TextField,
   Typography,
 } from "@mui/material";
 
-import { Cart, PERSONS, setGoodsIssueOrReception } from "../../state/cartSlice";
-
-const modalStyles = {
-  wrapper: {
-    position: "fixed",
-    top: "50%",
-    left: "50%",
-    transform: "translate(-50%, -50%)",
-    width: "max(500px, 30%)",
-    bgcolor: "white",
-    p: 4,
-    zIndex: 10,
-  },
-  inputFields: {
-    display: "flex",
-    flexDirection: "column",
-    marginTop: "20px",
-    marginBottom: "15px",
-    gap: "20px",
-  },
-  buttons: {
-    display: "flex",
-    justifyContent: "end",
-    gap: "10px",
-  },
-};
+import { Cart, PERSONS } from "../../state/cartSlice";
+import { Formik, FormikHelpers } from "formik";
 
 interface Props {
   open: boolean;
@@ -50,9 +21,9 @@ interface Props {
   setCartItem: (data: Cart) => void;
 }
 
-const defaultValue: Cart = {
+const initialValues: Cart = {
   name: "",
-  amount: parseInt(""),
+  amount: 0,
   person: PERSONS.knadolna,
   productId: "",
 };
@@ -60,123 +31,101 @@ const defaultValue: Cart = {
 const validationSchema = Yup.object().shape({
   amount: Yup.number()
     .typeError("Pole może zawierać tylko liczby")
+    .moreThan(0, "Podaj wartość wiekszą niż 0")
     .required("Podaj ilość"),
+  person: Yup.mixed<PERSONS>()
+    .oneOf(Object.values(PERSONS), "Wybierz osobę")
+    .required(),
 });
 
 export const SupplyForm = (props: Props) => {
-  const dispatch = useAppDispatch();
-  const [value, setValue] = useState<Cart>(defaultValue);
   const cartProduct = useAppSelector((state) => state.cart.cartProduct);
-  const cartType = useAppSelector((state) => state.cart.goodsIssueOrReception);
 
-  const handleChangeCartType = (event: SelectChangeEvent) => {
-    dispatch(
-      setGoodsIssueOrReception(
-        event.target.value as "goodsIssue" | "goodsReception"
-      )
-    );
-  };
-
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm({ resolver: yupResolver(validationSchema) });
-
-  const setCartItem = (data: Cart) => {
+  const handleAmountForm = async (
+    values: Cart,
+    onSubmitProps: FormikHelpers<Cart>
+  ) => {
     props.setCartItem({
-      ...data,
-      person: value.person,
-      name: `${cartProduct.name} - ${cartProduct.secondName}`,
+      ...values,
+      name: cartProduct.name,
       productId: cartProduct.id as string,
     });
+    onSubmitProps.resetForm();
   };
-
-  useEffect(() => {
-    if (props.open)
-      setValue({
-        ...defaultValue,
-        productId: cartProduct.id as string,
-      });
-  }, [props.open]);
 
   return (
     <Modal
       open={props.open}
       onClose={props.onClose}
-      aria-labelledby="modal-title"
+      aria-labelledby="Add amount form"
     >
-      <Box sx={modalStyles.wrapper}>
-        <Box display="flex" justifyContent="end" mb="15px">
-          <FormControl size="small" sx={{ width: "50%" }}>
-            <InputLabel id="cart-select-label">Koszyk</InputLabel>
-            <Select
-              labelId="cart-select-label"
-              id="cart-select"
-              value={cartType}
-              label="Koszyk"
-              onChange={handleChangeCartType}
-            >
-              <MenuItem value="goodsIssue">
-                <Typography variant="h6">Wydanie</Typography>
-              </MenuItem>
-              <MenuItem value="goodsReception">
-                <Typography variant="h6">Przyjęcie</Typography>
-              </MenuItem>
-            </Select>
-          </FormControl>
-        </Box>
-        <Divider />
-        <Box sx={modalStyles.inputFields}>
-          <Typography variant="h5">{`${cartProduct.name} ${cartProduct.secondName}`}</Typography>
-          <TextField
-            placeholder="Ilość"
-            label="Ilość"
-            required
-            {...register("amount")}
-            error={!!errors.amount}
-            helperText={errors.amount?.message as string}
-            type="number"
-            value={isNaN(value.amount) ? "" : value.amount}
-            onChange={(event) =>
-              setValue((prevState) => ({
-                ...prevState,
-                amount: Number(event.target.value),
-              }))
-            }
-          />
-          <FormControl size="small">
-            <InputLabel id="person-select-label">Osoba</InputLabel>
-            <Select
-              labelId="person-select-label"
-              id="person-select"
-              value={value?.person}
-              label="Osoba"
-              onChange={(event) =>
-                setValue((prevState) => ({
-                  ...prevState,
-                  person: event.target.value as PERSONS,
-                }))
-              }
-            >
-              {Object.entries(PERSONS).map(([key, value]) => (
-                <MenuItem key={key} value={value}>
-                  {value}
-                </MenuItem>
-              ))}
-            </Select>
-          </FormControl>
-        </Box>
-        <Box sx={modalStyles.buttons}>
-          <Button
-            variant="contained"
-            onClick={handleSubmit((d) => setCartItem(d as Cart))}
-          >
-            <span>Wyślij</span>
-          </Button>
-          <Button onClick={props.onClose}>Cancel</Button>
-        </Box>
-      </Box>
+      <Paper
+        sx={{
+          position: "fixed",
+          top: "50%",
+          left: "50%",
+          transform: "translate(-50%, -50%)",
+          width: "max(400px, 30%)",
+          margin: "0 auto",
+          padding: "20px",
+          gap: "20px",
+        }}
+      >
+        <Formik
+          onSubmit={handleAmountForm}
+          initialValues={initialValues}
+          validationSchema={validationSchema}
+        >
+          {({
+            values,
+            errors,
+            touched,
+            handleBlur,
+            handleChange,
+            handleSubmit,
+          }) => (
+            <form onSubmit={handleSubmit}>
+              <Box display="flex" flexDirection="column" gap="20px">
+                <Typography variant="h5">{cartProduct.name}</Typography>
+                <TextField
+                  label="Ilość"
+                  onBlur={handleBlur}
+                  onChange={handleChange}
+                  value={values.amount}
+                  name="amount"
+                  error={Boolean(touched.amount) && Boolean(errors.amount)}
+                  helperText={touched.amount && errors.amount}
+                  type="number"
+                />
+                <TextField
+                  label="Osoba"
+                  onBlur={handleBlur}
+                  onChange={handleChange}
+                  value={values.person}
+                  name="person"
+                  error={Boolean(touched.person) && Boolean(errors.person)}
+                  helperText={touched.person && errors.person}
+                  select
+                >
+                  {Object.entries(PERSONS).map(([key, value]) => (
+                    <MenuItem key={key} value={value}>
+                      {value}
+                    </MenuItem>
+                  ))}
+                </TextField>
+
+                {/* BUTTONS */}
+                <Box display="flex" justifyContent="space-between" gap="10px">
+                  <Button variant="contained" type="submit" fullWidth>
+                    Wyślij
+                  </Button>
+                  <Button onClick={props.onClose}>Cancel</Button>
+                </Box>
+              </Box>
+            </form>
+          )}
+        </Formik>
+      </Paper>
     </Modal>
   );
 };
