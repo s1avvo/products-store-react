e5d43c9d6ba8b6a0d8e488d596d942973308aa26
s1avo <s.nadolny@hotmail.com>
2023-02-26 18:40:37 +0100

    Updated a page not found.

--- src/views/NotFoundView.tsx
+++ src/views/NotFoundView.tsx
@@ -1,7 +1,20 @@
 import React from "react";
+import { Box, Typography } from "@mui/material";
 
 export const NotFoundView = () => (
-    <>
-        <h1>You are lost...</h1>
-    </>
+  <>
+    <Box display="block" textAlign="center">
+      <Typography
+        variant="h2"
+        fontWeight="bold"
+        textTransform="uppercase"
+        margin="20px"
+      >
+        404 - Page not found.
+      </Typography>
+      <Typography variant="h4">
+        Wygląda na to, że zabłądziłeś. ¯\_(ツ)_/¯
+      </Typography>
+    </Box>
+  </>
 );
