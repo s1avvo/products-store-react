aa7ad84e438d79dcde9d2d356538669bee1b4a7c
s1avo <s.nadolny@hotmail.com>
2023-02-26 15:35:55 +0100

    Added snackbars to inform the users of cart behaviors.

--- src/views/Global/Cart.tsx
+++ src/views/Global/Cart.tsx
@@ -27,6 +27,10 @@ import {
 } from "../../state/cartSlice";
 
 import { setStatus, updateQty } from "../../state/productListSlice";
+import {
+  MySnackbar,
+  SnackbarInterface,
+} from "../../components/Global/MySnackbar";
 
 const FlexBox = styled(Box)`
   display: flex;
@@ -42,6 +46,11 @@ export const Cart = () => {
   const cartType = useAppSelector((state) => state.cart.goodsIssueOrReception);
   const isCartOpen = useAppSelector((state) => state.cart.isCartOpen);
   const [check, setCheck] = useState(false);
+  const [openSnackbar, setOpenSnackbar] = useState<SnackbarInterface>({
+    open: false,
+    alert: "",
+    variant: "info",
+  });
 
   const handleChangeCartType = (event: SelectChangeEvent) => {
     dispatch(
@@ -53,8 +62,6 @@ export const Cart = () => {
 
   const handelGoodsReception = (event: FormEvent) => {
     event.preventDefault();
-
-    console.log(cart);
 
     cart.map(async (item) => {
       try {
@@ -74,9 +81,18 @@ export const Cart = () => {
         dispatch(updateQty({ id: item.productId, qty: item.amount }));
         dispatch(clearCart());
       } catch (err) {
-        console.error(err);
+        setOpenSnackbar({
+          open: true,
+          alert: `Nie powiodło się przyjęcie produktu na stan, spróbuj ponownie.`,
+          variant: "error",
+        });
       } finally {
         dispatch(setStatus("idle"));
+        setOpenSnackbar({
+          open: true,
+          alert: `Przyjęto na stan ${item.name} - ${item.amount}`,
+          variant: "success",
+        });
       }
     });
   };
@@ -102,9 +118,18 @@ export const Cart = () => {
         dispatch(updateQty({ id: item.productId, qty: -item.amount }));
         dispatch(clearCart());
       } catch (err) {
-        console.error(err);
+        setOpenSnackbar({
+          open: true,
+          alert: `Nie powiodło się wydanie produktu, spróbuj ponownie.`,
+          variant: "error",
+        });
       } finally {
         dispatch(setStatus("idle"));
+        setOpenSnackbar({
+          open: true,
+          alert: `Wydano ${item.name} - ${item.amount}`,
+          variant: "success",
+        });
       }
     });
   };
@@ -114,104 +139,120 @@ export const Cart = () => {
   }, [isCartOpen]);
 
   return (
-    <Box
-      display={isCartOpen ? "block" : "none"}
-      bgcolor="rgba(0, 0, 0, 0.4)"
-      position="fixed"
-      zIndex={10}
-      width="100%"
-      height="100%"
-      left="0"
-      top="0"
-      overflow="auto"
-    >
+    <>
       <Box
-        position="absolute"
-        right="0px"
-        margin="0 auto"
-        width={isNonMobileScreens ? "max(420px, 30%)" : "100%"}
+        display={isCartOpen ? "block" : "none"}
+        bgcolor="rgba(0, 0, 0, 0.4)"
+        position="fixed"
+        zIndex={10}
+        width="100%"
         height="100%"
-        bgcolor="white"
-        component="form"
-        onSubmit={
-          cartType === "goodsIssue" ? handelGoodsIssue : handelGoodsReception
-        }
+        left="0"
+        top="0"
+        overflow="auto"
       >
-        <Box padding="30px" overflow="auto" height="100%">
-          {/* HEADER */}
-          <FlexBox mb="15px">
-            <FormControl size="small" fullWidth>
-              <InputLabel id="cart-select-label">Koszyk</InputLabel>
-              <Select
-                labelId="cart-select-label"
-                id="cart-select"
-                value={cartType}
-                label="Koszyk"
-                onChange={handleChangeCartType}
+        <Box
+          position="absolute"
+          right="0px"
+          margin="0 auto"
+          width={isNonMobileScreens ? "max(420px, 30%)" : "100%"}
+          height="100%"
+          bgcolor="white"
+          component="form"
+          onSubmit={
+            cartType === "goodsIssue" ? handelGoodsIssue : handelGoodsReception
+          }
+        >
+          <Box padding="30px" overflow="auto" height="100%">
+            {/* HEADER */}
+            <FlexBox mb="15px">
+              <FormControl size="small" fullWidth>
+                <InputLabel id="cart-select-label">Koszyk</InputLabel>
+                <Select
+                  labelId="cart-select-label"
+                  id="cart-select"
+                  value={cartType}
+                  label="Koszyk"
+                  onChange={handleChangeCartType}
+                >
+                  <MenuItem value="goodsIssue">
+                    <Typography variant="h5">
+                      Wydanie ({cart.length})
+                    </Typography>
+                  </MenuItem>
+                  <MenuItem value="goodsReception">
+                    <Typography variant="h5">
+                      Przyjęcie ({cart.length})
+                    </Typography>
+                  </MenuItem>
+                </Select>
+              </FormControl>
+              <Tooltip title="Sprawdź typ koszyka i potwierdź przed wysłaniem.">
+                <Checkbox
+                  checked={check}
+                  onChange={() => setCheck(!check)}
+                  inputProps={{ "aria-label": "controlled" }}
+                  sx={{ "& .MuiSvgIcon-root": { fontSize: "2.5rem" } }}
+                />
+              </Tooltip>
+            </FlexBox>
+
+            {/* CART LIST */}
+            {cart.map((item) => (
+              <Box key={`${item.productId}`}>
+                <FlexBox p="15px 0">
+                  <Box flex="1 1 60%">
+                    <FlexBox mb="5px" mt="5px">
+                      <Typography fontWeight="bold" variant="h5">
+                        {item.name}
+                      </Typography>
+                      <IconButton
+                        onClick={() =>
+                          dispatch(
+                            removeFromCart({ productId: item.productId })
+                          )
+                        }
+                      >
+                        <CloseIcon />
+                      </IconButton>
+                    </FlexBox>
+                    <FlexBox m="15px 0">
+                      <Typography variant="h6">
+                        Ilość: <b>{item.amount}</b>
+                      </Typography>
+                      <Typography>{item.person}</Typography>
+                    </FlexBox>
+                  </Box>
+                </FlexBox>
+                <Divider />
+              </Box>
+            ))}
+            {/*BUTTONS*/}
+
+            <Box m="20px 0" display="flex" justifyContent="end" gap="10px">
+              <Button
+                variant="contained"
+                type="submit"
+                onClick={() => dispatch(setIsCartOpen())}
+                disabled={!check}
               >
-                <MenuItem value="goodsIssue">
-                  <Typography variant="h5">Wydanie ({cart.length})</Typography>
-                </MenuItem>
-                <MenuItem value="goodsReception">
-                  <Typography variant="h5">
-                    Przyjęcie ({cart.length})
-                  </Typography>
-                </MenuItem>
-              </Select>
-            </FormControl>
-            <Tooltip title="Sprawdź typ koszyka i potwierdź przed wysłaniem.">
-              <Checkbox
-                checked={check}
-                onChange={() => setCheck(!check)}
-                inputProps={{ "aria-label": "controlled" }}
-                sx={{ "& .MuiSvgIcon-root": { fontSize: "2.5rem" } }}
-              />
-            </Tooltip>
-          </FlexBox>
-
-          {/* CART LIST */}
-          {cart.map((item) => (
-            <Box key={`${item.productId}`}>
-              <FlexBox p="15px 0">
-                <Box flex="1 1 60%">
-                  <FlexBox mb="5px" mt="5px">
-                    <Typography fontWeight="bold" variant="h5">
-                      {item.name}
-                    </Typography>
-                    <IconButton
-                      onClick={() =>
-                        dispatch(removeFromCart({ productId: item.productId }))
-                      }
-                    >
-                      <CloseIcon />
-                    </IconButton>
-                  </FlexBox>
-                  <FlexBox m="15px 0">
-                    <Typography variant="h6">
-                      Ilość: <b>{item.amount}</b>
-                    </Typography>
-                    <Typography>{item.person}</Typography>
-                  </FlexBox>
-                </Box>
-              </FlexBox>
-              <Divider />
+                Wyślij
+              </Button>
+              <Button onClick={() => dispatch(setIsCartOpen())}>Anuluj</Button>
             </Box>
-          ))}
-          {/*BUTTONS*/}
-
-          <Box m="20px 0" display="flex" justifyContent="end" gap="10px">
-            <Button
-              variant="contained"
-              type="submit"
-              onClick={() => dispatch(setIsCartOpen())}
-              disabled={!check}
-            >
-              Wyślij
-            </Button>
-            <Button onClick={() => dispatch(setIsCartOpen())}>Anuluj</Button>
           </Box>
         </Box>
       </Box>
-    </Box>
+      {openSnackbar && (
+        <MySnackbar
+          open={openSnackbar.open}
+          onClose={() =>
+            setOpenSnackbar((prevState) => ({ ...prevState, open: false }))
+          }
+          alert={openSnackbar.alert}
+          variant={openSnackbar.variant}
+        />
+      )}
+    </>
   );
 };
